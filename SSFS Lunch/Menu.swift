9949caf3d8877c6b08391b3e0f36d8f5eace7f06
Claire Youmans <claireyoumans@Claires-MacBook-Pro.local>
2016-04-25 11:01:23 -0400

    added Menu class

---
+++ SSFS Lunch/Menu.swift
@@ -0,0 +1,12 @@
+//
+//  Menu.swift
+//  SSFS Lunch
+//
+//  Created by Claire Youmans on 4/25/16.
+//  Copyright © 2016 Claire Youmans. All rights reserved.
+//
+
+import Foundation
+
+
+//makes a menu class
